79687df2a5ccfb0eb2a71c220d4ac6a5ff86b56c
Theadedamola <adedamolajose@gmail.com>
2026-02-03 21:42:22 +0100

    feat: Centralize SEO management by adding a dedicated component and removing static meta tags from `index.html`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './app/router';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
+import SEO from "./components/common/SEO";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +16,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <SEO />
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
