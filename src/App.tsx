2a0b936f6844178b232ea2d59805f9b042dbd1d2
Theadedamola <adedamolajose@gmail.com>
2026-03-05 00:23:05 +0100

    feat: Add comprehensive SEO meta tags to `index.html` and remove the `SEO` component from `App.tsx`.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
-import SEO from "./components/common/SEO";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +15,6 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <SEO />
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
